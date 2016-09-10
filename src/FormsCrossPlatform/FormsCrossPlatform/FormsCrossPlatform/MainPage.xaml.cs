1b3130e932e2b8de7fa6380e60621b8fe879c3fc
Gyuwon Yi <gyuwon@live.com>
2016-09-10 16:40:40 +0900

    FormsCrossPlatform 응용프로그램 논리를 구현합니다.

--- src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform/MainPage.xaml.cs
+++ src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform/MainPage.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace FormsCrossPlatform
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ObservableCollection<Contact> _contacts;
+
         public MainPage()
         {
             InitializeComponent();
+
+            _contacts = new ObservableCollection<Contact>();
+
+            _contactList.ItemsSource = _contacts;
+
+            _add.Clicked += delegate
+            {
+                if (string.IsNullOrWhiteSpace(_name.Text) ||
+                    string.IsNullOrWhiteSpace(_email.Text))
+                    return;
+
+                _contacts.Add(new Contact(_name.Text, _email.Text));
+
+                _name.Text = string.Empty;
+                _email.Text = string.Empty;
+            };
         }
     }
 }
