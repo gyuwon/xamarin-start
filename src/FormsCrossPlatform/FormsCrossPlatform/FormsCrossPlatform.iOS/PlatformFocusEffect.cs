d62e052716c32a0ba2490755945ce6a80a07ec22
Gyuwon Yi <gyuwon@live.com>
2016-09-10 16:59:41 +0900

    FocusEffect 효과의 iOS 구현체를 추가합니다.

---
+++ src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform.iOS/PlatformFocusEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using FormsCrossPlatform;
+using FormsCrossPlatform.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ResolutionGroupName(nameof(FormsCrossPlatform))]
+[assembly:ExportEffect(typeof(PlatformFocusEffect), nameof(FocusEffect))]
+
+namespace FormsCrossPlatform.iOS
+{
+    public class PlatformFocusEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(
+            PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == nameof(Entry.IsFocused))
+            {
+                var entry = (Entry)Element;
+                Control.BackgroundColor =
+                    entry.IsFocused
+                    ? UIColor.Yellow
+                    : UIColor.White;
+            }
+        }
+    }
+}
