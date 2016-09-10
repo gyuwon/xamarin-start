8914970d0fc79e25ec5c8783e868491ff1efaa17
Gyuwon Yi <gyuwon@live.com>
2016-09-10 16:57:38 +0900

    FormsCrossPlatform 프로젝트에 FocusEffect 클래스를 추가합니다.

---
+++ src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform/FocusEffect.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FormsCrossPlatform
+{
+    public class FocusEffect : RoutingEffect
+    {
+        public FocusEffect()
+            : base($"{nameof(FormsCrossPlatform)}.{nameof(FocusEffect)}")
+        {
+        }
+    }
+}
