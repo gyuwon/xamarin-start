56a1dc8366aa96cdb89080a1d2cc28b7726a8e9a
Gyuwon Yi <gyuwon@live.com>
2016-09-10 18:08:46 +0900

    FormsCrossPlatform 응용프로그램 논리를 MVVM 패턴을 사용해 리팩터합니다.

---
+++ src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform.ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace FormsCrossPlatform.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string _name;
+        private string _email;
+
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+
+            Add = new RelayCommand(p =>
+            {
+                if (string.IsNullOrWhiteSpace(Name) ||
+                    string.IsNullOrWhiteSpace(Email))
+                    return;
+
+                Contacts.Add(new Contact(Name, Email));
+
+                Name = string.Empty;
+                Email = string.Empty;
+            });
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { Set(ref _name, value); }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { Set(ref _email, value); }
+        }
+
+        public ObservableCollection<Contact> Contacts { get; }
+
+        public ICommand Add { get; }
+    }
+}
--- src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform/MainPage.xaml.cs
+++ src/FormsCrossPlatform/FormsCrossPlatform/FormsCrossPlatform/MainPage.xaml.cs
@@ -1,31 +1,12 @@
-﻿using System.Collections.ObjectModel;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace FormsCrossPlatform
 {
     public partial class MainPage : ContentPage
     {
-        private readonly ObservableCollection<Contact> _contacts;
-
         public MainPage()
         {
             InitializeComponent();
-
-            _contacts = new ObservableCollection<Contact>();
-
-            _contactList.ItemsSource = _contacts;
-
-            _add.Clicked += delegate
-            {
-                if (string.IsNullOrWhiteSpace(_name.Text) ||
-                    string.IsNullOrWhiteSpace(_email.Text))
-                    return;
-
-                _contacts.Add(new Contact(_name.Text, _email.Text));
-
-                _name.Text = string.Empty;
-                _email.Text = string.Empty;
-            };
         }
     }
 }
